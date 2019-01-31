c058b2efc33462e2715e15fa19a17ced200b61bf
natal36 <natalia.sykes81@outlook.com>
2019-01-31 16:57:02 +0000

    Update weather_function.py

--- weather_function.py
+++ weather_function.py
@@ -6,5 +6,7 @@ def get_current_weather(address):
   api_key = " "
   forecast = forecastio.load_forecast(api_key, location.latitude, location.longitude). currently()
   return f"It is currently {forecast.cummary} and {forecast.temperatur} degrees in {location.address}"
-
+  else:
+  return"Location not found"
+  
 print(get_current_weather("New York, NY"))
