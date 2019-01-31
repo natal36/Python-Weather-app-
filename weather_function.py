68e1e2b66fbb65137e6b92193e85656467798339
natal36 <natalia.sykes81@outlook.com>
2019-01-31 16:52:34 +0000

    Create weather_function.py

---
+++ weather_function.py
@@ -0,0 +1,10 @@
+import forecastio
+from geopy.geocoders import Nominatim
+
+def get_current_weather(address):
+  location = Nominatim.geocode(address) 
+  api_key = " "
+  forecast = forecastio.load_forecast(api_key, location.latitude, location.longitude). currently()
+  return f"It is currently {forecast.cummary} and {forecast.temperatur} degrees in {location.address}"
+
+print(get_current_weather("New York, NY"))
